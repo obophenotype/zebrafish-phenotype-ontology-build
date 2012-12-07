644d6f74e79e0517c5819e0f943434f60e677dd4
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-07 21:55:09 +0000

    Added zpid registry and a test (as lazy as I am in the main source which doesn't make sense)

---
+++ zp/src/ZPIDDB.java
@@ -0,0 +1,104 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.coode.owlapi.obo.parser.OBOVocabulary;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * Simple zp id registry.
+ * 
+ * @author Sebastian Bauer
+ */
+public class ZPIDDB
+{
+	private static Logger log = Logger.getAnonymousLogger();
+
+	/** Next id to be assigned in case of an unknown class expression */
+	private int nextId = 1;
+
+	/** Every class expression got's an own distinct id, which is stored here */
+	private HashMap<OWLClassExpression,IRI> class2Id = new HashMap<OWLClassExpression,IRI>();
+
+	/**
+	 * Constructs an empty zp id database. 
+	 */
+	public ZPIDDB()
+	{
+		this(null);
+	}
+
+	/**
+	 * Constructs a zp id data base and filling it
+	 * with previously assigned ids gathered from
+	 * the given ontology.
+	 *  
+	 * @param zp
+	 */
+	public ZPIDDB(OWLOntology zp)
+	{
+		if (zp != null)
+		{
+			for (OWLAxiom ax : zp.getAxioms(AxiomType.EQUIVALENT_CLASSES))
+			{
+				if (ax instanceof OWLEquivalentClassesAxiom)
+				{
+					OWLEquivalentClassesAxiom eq = (OWLEquivalentClassesAxiom) ax;
+					List<OWLClassExpression> exprList = eq.getClassExpressionsAsList();
+					if (exprList.size() != 2)
+					{
+						log.warning("Unknown format in equivalence axiom: " + eq);
+						continue;
+					}
+					OWLClassExpression cl1 = exprList.get(0);
+					OWLClassExpression cl2 = exprList.get(1);
+
+					OWLClass zpClass = null;
+
+					if (cl1 instanceof OWLClass) zpClass = (OWLClass)cl1;
+					else
+					{
+						log.warning("Unknown format in equivalence axiom: " + eq);
+						continue;						
+					}
+					
+					IRI zpIRI = zpClass.getIRI();
+					String zpID = OBOVocabulary.IRI2ID(zpIRI);
+					if (!zpID.startsWith("ZP:"))
+					{
+						log.warning("Unknown term name in equivalence axiom: " + eq);
+						continue;						
+					}
+					int id = Integer.parseInt(zpID.substring(3));
+					if (id >= nextId) nextId = id + 1;
+
+					class2Id.put(cl2, zpIRI);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the Id (as IRI) of the given owlSomeClassExp.
+	 * This may be a new one if classExpression was not seen
+	 * before.
+	 * 
+	 * @param owlSomeClassExp
+	 * @return
+	 */
+	public IRI getZPId(OWLClassExpression classExpression)
+	{
+		IRI zpIdIRI = class2Id.get(classExpression);
+		if (zpIdIRI != null) return zpIdIRI;
+		String zpId = String.format("ZP:%07d",nextId++);
+		zpIdIRI = OBOVocabulary.ID2IRI(zpId);
+		class2Id.put(classExpression, zpIdIRI);
+		return zpIdIRI;
+	}
+}
---
+++ zp/src/ZPIDDBTest.java
@@ -0,0 +1,93 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+
+import junit.framework.Assert;
+
+import org.coode.owlapi.obo.parser.OBOVocabulary;
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+
+/**
+ * Tests ZPIDDB.
+ * 
+ * @author Sebastian Bauer
+ */
+public class ZPIDDBTest
+{
+	@Test
+	public void testZPIDDB() throws OWLOntologyStorageException, OWLOntologyCreationException
+	{
+		/* Create a small test ontology */
+		final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		final OWLDataFactory factory = manager.getOWLDataFactory();
+		final IRI zpIRI = IRI.create("http://purl.obolibrary.org/obo/");
+		final OWLObjectProperty partOf = factory.getOWLObjectProperty(IRI.create(zpIRI + "BFO_0000050"));
+
+		final OWLOntology testOntology = manager.createOntology(IRI.create("test"));
+		
+		ZPIDDB zpIdDB = new ZPIDDB();
+
+		OWLClass pato 	= factory.getOWLClass(IRI.create("PATO:1"));
+		OWLClass cl1 	= factory.getOWLClass(IRI.create("TEST:1"));
+		OWLClass cl1a 	= factory.getOWLClass(IRI.create("TEST:1"));
+		OWLClass cl2 	= factory.getOWLClass(IRI.create("TEST:2"));
+		OWLClass cl3 	= factory.getOWLClass(IRI.create("TEST:3"));
+		
+		OWLClassExpression expr1 = factory.getOWLObjectIntersectionOf(cl1,factory.getOWLObjectSomeValuesFrom(partOf, cl2));
+		IRI zp1IRI = zpIdDB.getZPId(expr1);
+		Assert.assertEquals(OBOVocabulary.ID2IRI("ZP:0000001"),zp1IRI);
+		manager.addAxiom(testOntology,factory.getOWLEquivalentClassesAxiom(factory.getOWLClass(zp1IRI),expr1));
+
+		OWLClassExpression expr2 = factory.getOWLObjectIntersectionOf(cl1,factory.getOWLObjectSomeValuesFrom(partOf, cl3));
+		IRI zp2IRI = zpIdDB.getZPId(expr2);
+		Assert.assertEquals(OBOVocabulary.ID2IRI("ZP:0000002"),zp2IRI);
+		manager.addAxiom(testOntology,factory.getOWLEquivalentClassesAxiom(factory.getOWLClass(zp2IRI),expr2));
+
+		OWLClassExpression expr1a = factory.getOWLObjectIntersectionOf(cl1a,factory.getOWLObjectSomeValuesFrom(partOf, cl2));
+		IRI zp1aIRI = zpIdDB.getZPId(expr1a);
+		Assert.assertEquals(OBOVocabulary.ID2IRI("ZP:0000001"),zp1aIRI);
+		manager.addAxiom(testOntology,factory.getOWLEquivalentClassesAxiom(factory.getOWLClass(zp1IRI),expr1a));
+
+		OWLClassExpression expr3 = factory.getOWLObjectIntersectionOf(pato,cl1,factory.getOWLObjectSomeValuesFrom(partOf, cl3));
+		Assert.assertEquals(OBOVocabulary.ID2IRI("ZP:0000003"),zpIdDB.getZPId(expr3));
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		manager.saveOntology(testOntology, baos);
+		manager.saveOntology(testOntology, System.out);
+		byte [] output = baos.toByteArray();
+
+		/* And now the second pass, i.e., load the ontology again and create some old and new classes */
+		ByteArrayInputStream bais = new ByteArrayInputStream(output);
+		final OWLOntologyManager manager2 = OWLManager.createOWLOntologyManager();
+		OWLOntology testOntology2 = manager2.loadOntologyFromOntologyDocument(bais);
+		
+		ZPIDDB zpIdDB2 = new ZPIDDB(testOntology2);
+		OWLDataFactory factory2 = manager.getOWLDataFactory();
+		OWLClass cl21 	= factory2.getOWLClass(IRI.create("TEST:1"));
+		OWLClass cl21a 	= factory2.getOWLClass(IRI.create("TEST:1"));
+		OWLClass cl22 	= factory2.getOWLClass(IRI.create("TEST:2"));
+		OWLClass cl23 	= factory2.getOWLClass(IRI.create("TEST:3"));
+
+		final OWLObjectProperty partOf2 = factory2.getOWLObjectProperty(IRI.create(zpIRI + "BFO_0000050"));
+
+		OWLClassExpression expr22 = factory2.getOWLObjectIntersectionOf(cl1,factory.getOWLObjectSomeValuesFrom(partOf, cl23));
+		Assert.assertEquals(OBOVocabulary.ID2IRI("ZP:0000002"),zpIdDB2.getZPId(expr22));
+
+		OWLClassExpression expr21 = factory2.getOWLObjectIntersectionOf(cl21,factory.getOWLObjectSomeValuesFrom(partOf2, cl22));
+		Assert.assertEquals(zp1IRI,zpIdDB2.getZPId(expr21));
+		
+
+	}
+}

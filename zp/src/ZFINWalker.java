0f044e5302ff4e286cbb4a78e48577ea0f693afb
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-09 07:48:15 +0000

    Adapted to the current format.

--- zp/src/ZFINWalker.java
+++ zp/src/ZFINWalker.java
@@ -7,22 +7,7 @@ import java.io.InputStreamReader;
  * Provides a method to walkes a ZFIN file.
  * Calls {@link ZFINVisitor#visit(ZFINEntry)} for each encountered entry.
  * 
- * 	0 ZDB-GENE-030131-7487
-	1 563421
-	2 154796
-	3 amot4
-	4 ZFA:0001285
-	5 intersegmental vessel
-	6 ZFA:0009036
-	7 blood vessel endothelial cell
-	8 ZFA:0001285
-	9 intersegmental vessel
-	10 ZFA:0009036
-	11 blood vessel endothelial cell
-	12 PATO:0001531
-	13 cellular adhesivity
-	14 abnormal
-	15
+ * See http://zfin.org/downloads for a current format.
  * 
  * @author Sebastian Bauer
  */
@@ -32,18 +17,18 @@ public class ZFINWalker
 	
 	static int COLUMN_ZFIN_GENE_ID 			= 0;
 
-	static int COLUMN_TERM1_SUPERTERM_ID 	= 4;
-	static int COLUMN_TERM1_SUPERTERM_NAME 	= 5;
-	static int COLUMN_TERM1_SUBTERM_ID 		= 6;
-	static int COLUMN_TERM1_SUBTERM_NAME 	= 7;	
-	static int COLUMN_TERM2_SUPERTERM_ID 	= 8;
-	static int COLUMN_TERM2_SUPERTERM_NAME 	= 9;
-	static int COLUMN_TERM2_SUBTERM_ID 		= 10;
-	static int COLUMN_TERM2_SUBTERM_NAME 	= 11;
+	static int COLUMN_TERM1_SUPERTERM_ID 	= 6;
+	static int COLUMN_TERM1_SUPERTERM_NAME 	= 7;
+	static int COLUMN_TERM1_SUBTERM_ID 		= 8;
+	static int COLUMN_TERM1_SUBTERM_NAME 	= 9;	
+	static int COLUMN_TERM2_SUPERTERM_ID 	= 13;
+	static int COLUMN_TERM2_SUPERTERM_NAME 	= 14;
+	static int COLUMN_TERM2_SUBTERM_ID 		= 15;
+	static int COLUMN_TERM2_SUBTERM_NAME 	= 16;
 
-	static int COLUMN_PATO_ID 				= 12;
-	static int COLUMN_PATO_NAME 				= 13;
-	static int COLUMN_PATO_ABNORMAL_NORMAL 	= 14;
+	static int COLUMN_PATO_ID 				= 10;
+	static int COLUMN_PATO_NAME 			= 11;
+	static int COLUMN_PATO_MODIFIER 		= 12;
 	
 	static public void walk(InputStream input, ZFINVisitor visitor) throws IOException
 	{
@@ -73,7 +58,8 @@ public class ZFINWalker
 	
 				entry.patoID 		= sp[COLUMN_PATO_ID];
 				entry.patoName 		= sp[COLUMN_PATO_NAME];
-				entry.isAbnormal 	= sp[COLUMN_PATO_ABNORMAL_NORMAL].equalsIgnoreCase("abnormal");
+				entry.isAbnormal 	= sp[COLUMN_PATO_MODIFIER].equalsIgnoreCase("abnormal");
+	
 				visitor.visit(entry);
 			}
 			catch (Exception e) {

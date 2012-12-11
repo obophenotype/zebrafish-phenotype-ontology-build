825e5906f7ec0e27bfb3e2e5f37cc14fbbb61e8e
Sebastian Bauer <mail@sebastianbauer.info>
2012-12-11 13:49:35 +0000

    The ontology name is now zp.

--- zpgen/src/main/java/de/sonumina/zpgen/ZPGen.java
+++ zpgen/src/main/java/de/sonumina/zpgen/ZPGen.java
@@ -64,7 +64,7 @@ public class ZPGen
 				
 		/* Create ontology manager and IRIs */
 		final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		final IRI zpIRI = IRI.create("http://purl.obolibrary.org/obo/");
+		final IRI zpIRI = IRI.create("http://purl.obolibrary.org/obo/zp");
 
 		/* Load the previous zp, if requested */
 		final OWLOntology zp;

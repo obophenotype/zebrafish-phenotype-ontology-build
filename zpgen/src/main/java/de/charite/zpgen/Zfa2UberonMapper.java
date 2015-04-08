7ddc84978bcfe44f9f94d463bf039a136f89ce68
Sebastian Köhler <dr.sebastian.koehler@gmail.com>
2015-04-08 16:59:44 +0200

    forgot the mapper-class

---
+++ zpgen/src/main/java/de/charite/zpgen/Zfa2UberonMapper.java
@@ -0,0 +1,16 @@
+package de.charite.zpgen;
+
+import java.util.HashMap;
+
+public class Zfa2UberonMapper {
+
+	public Zfa2UberonMapper(String uberonOboFilePath) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashMap<String, String> getZfa2UberonMapping() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

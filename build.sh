61e0906a1df0506cb57d4d5b8bb9c80d7738cf84
Sebastian <dr.sebastian.koehler@gmail.com>
2017-07-05 14:35:45 +0200

    move data and build-script

--- build.sh
+++ build.sh
@@ -16,9 +16,9 @@ wget -N http://zfin.org/downloads/phenoGeneCleanData_fish.txt
 wget -N http://zfin.org/downloads/phenotype_fish.txt
 
 
-mvn clean install package
+mvn -f zpgen/ clean install package
 
 
 # run the normal build 
-java -jar target/zp-0.1-SNAPSHOT-jar-with-dependencies.jar --zfin-pheno-txt-input-file phenoGeneCleanData_fish.txt --zfin-phenotype-txt-input-file phenotype_fish.txt -p zp_previous.owl -o zp.owl -a ./ --add-source-information -s zp.annot_sourceinfo --keep-ids
+java -jar zpgen/target/zp-0.1-SNAPSHOT-jar-with-dependencies.jar --zfin-pheno-txt-input-file phenoGeneCleanData_fish.txt --zfin-phenotype-txt-input-file phenotype_fish.txt -p zp_previous.owl -o zp.owl -a ./ --add-source-information -s zp.annot_sourceinfo --keep-ids
 
